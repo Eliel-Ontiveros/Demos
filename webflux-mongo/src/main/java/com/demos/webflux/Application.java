7f20c2a124e7f6f0a4e3592d4c352184e96d3a82
Eliel-Ontiveros <141880514+Eliel-Ontiveros@users.noreply.github.com>
2026-04-27 10:12:50 -0700

    fix: exclude all MongoDB auto-configurations to fix startup error

--- webflux-mongo/src/main/java/com/demos/webflux/Application.java
+++ webflux-mongo/src/main/java/com/demos/webflux/Application.java
@@ -2,10 +2,19 @@ package com.demos.webflux;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoReactiveDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoReactiveRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoReactiveAutoConfiguration;
 
-@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoReactiveAutoConfiguration.class})
+@SpringBootApplication(exclude = {
+    MongoAutoConfiguration.class,
+    MongoReactiveAutoConfiguration.class,
+    MongoDataAutoConfiguration.class,
+    MongoReactiveDataAutoConfiguration.class,
+    MongoReactiveRepositoriesAutoConfiguration.class
+})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
